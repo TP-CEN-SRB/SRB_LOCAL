97251e33fe257bb14b618ba8b232c064fe9f9944
PiersChua <2201999F@student.tp.edu.sg>
2025-02-05 11:23:38 +0800

    changed environment variables

--- keys.ts
+++ keys.ts
@@ -1,12 +1,12 @@
 const BASE_URL = "http://localhost:3000";
-const HOSTED_URL = "https://major-project-tp.vercel.app";
+const HOSTED_URL = "https://cen-smart-bin.vercel.app";
 
 const API_KEY =
   "oiKWuSpfjx7NZjU85bgdx7HXPqUCIsOv8ghauPXC8b1cfAq5QLdkg7rqaUskCinuj1ebHogbqBaIgkIZ0H9U6labkVe6AmtIScQWy5YV0hGasKA43wKL2OztPMVXg9ZWndKdf1Gc9I8t2mZEOv2tDF2cbgD23x1mzYFEuTgkilczcPJSoLQYfoyw12XMhsMbuhz3FKDvipLjfSJunTogmeOJN262NmoKzBAeOk4CDJRNkAkX1QlaBfvyTVdgvg6g";
 
-const PUSHER_APP_ID = "1889307";
-const PUSHER_KEY = "3d6fb36984d85ee78c0a";
-const PUSHER_SECRET = "d4deea9dbcd45c04baa3";
+const PUSHER_APP_ID = "1936854";
+const PUSHER_KEY = "1cd96e25757d6442dbf0";
+const PUSHER_SECRET = "efcd3d842efe3624a4f4";
 const PUSHER_CLUSTER = "ap1";
 
 const NEXT_JWT_SECRET_KEY = "auxVEBZpsf01YJYR/drUwMiDmqOzWywkTFfz9ogDMKU=";
